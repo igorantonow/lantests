5d926bc9ec9eb8f947ac3440f6ebaaac98b8f640
igorantonow <cluchikzol@mail.ru>
2018-12-11 19:21:19 +0300

    Deleted dublicate of code in statistics.py

--- serv.py
+++ serv.py
@@ -6,48 +6,6 @@ import statistic as stat
 
 WORK_DIRECTORY = r'/sdcard/user/tests/wrkdir'
 
-#-----statistics operating-----
-'''def addStatRec(record):
-    if (not os.path.isfile("stat.pkl")):
-        f = open("stat.pkl", "wb")
-        pkl.dump(["format=my"], f)
-        f.close()
-    f = open("stat.pkl", "rb")
-    recs = pkl.load(f)
-    f.close()
-    f = open("stat.pkl", "wb")
-    recs.append(record)
-    pkl.dump(recs, f)
-    f.close()
-
-def loadStat():
-    if (not os.path.isfile("stat.pkl")):
-        f = open("stat.pkl", "wb")
-        pkl.dump(["format=my"], f)
-        f.close()
-    f = open("stat.pkl", "rb")
-    stat = pkl.load(f)
-    f.close()
-    return stat
-
-def getLastTestRes(name):
-    print("getLastTestRes("+name+')')
-    stat = loadStat()
-    stat.reverse()
-    for record in stat:
-        if record[0] == name:
-            return record
-    raise ValueError("cannot find records about "+name)
-
-def oformResTest(name):
-    res = getLastTestRes(name)
-    mistakes = [ i+1 for i in res[2] ]
-    text = "Congratulations! You have "+\
-           str(res[1]*100//len(getAnsws()))+\
-           "% correct answers. Your mistakes:"+\
-           str(mistakes)
-    return text.encode("utf-8")
-'''
 #-------test operating---------
 def saveTest(tst):
     f = open("test.pkl", 'wb')
